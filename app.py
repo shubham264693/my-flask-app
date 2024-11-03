b65541ee0f6d152effefe2fc7334fdd31259100a
=sudhanshuvlog1 <pandeysudhanshuup@gmail.com>
2024-11-03 16:05:16 +0530

    updated appy

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask import Flask
 from urllib.parse import quote 
 
 def create_app():
+    x=10
+    y=100
+    z=30
     app = Flask(__name__)
 
     @app.route('/')
@@ -11,6 +14,17 @@ def create_app():
         return 'Wow CI-CD Pipeline is been developed updated Hurrayyyyy..'
 
     return app
+
+def test():
+    print("hi")
+    test()
+
+def sum(z,y):
+    return z+y
+
+def add(a,b):
+    return a+b
+
 if __name__ == '__main__':
     app = create_app()
     app.run(host='0.0.0.0', port=80, debug=True)
